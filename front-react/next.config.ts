a3779c89618724781a0aef616913699aa4cc5867
asamigentoku <gentoku.asami.9v@stu.hosei.ac.jp>
2026-05-26 21:15:18 +0900

    envの読み取り方法を変更

--- front-react/next.config.ts
+++ front-react/next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  env: {
-    REACT_APP_FIREBASE_API_KEY:            process.env.REACT_APP_FIREBASE_API_KEY,
-    REACT_APP_FIREBASE_AUTH_DOMAIN:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    REACT_APP_FIREBASE_PROJECT_ID:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    REACT_APP_FIREBASE_STORAGE_BUCKET:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    REACT_APP_FIREBASE_MESSAGING_SENDER_ID: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    REACT_APP_FIREBASE_APP_ID:             process.env.REACT_APP_FIREBASE_APP_ID,
-  },
 };
 
 export default nextConfig;
